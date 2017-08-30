aa389d4bb86e271e2726d4af8e59f6de728f9d47
Aucher Serr <serra774@newschool.edu>
2017-08-30 17:10:37 +0000

    terminate statements

--- assignment1/week01.js
+++ assignment1/week01.js
@@ -1,6 +1,6 @@
 var request = require('request');
 var fs = require('fs');
-var base_url = 'http://visualizedata.github.io/datastructures/data/'
+var base_url = 'http://visualizedata.github.io/datastructures/data/';
 
 var file = ['m01','m02','m03', 'm04', 'm05', 'm06', 'm07', 'm08', 'm09', 'm10'];
 
@@ -13,7 +13,7 @@ file.forEach (function(f,i) {
         fs.writeFileSync('/home/ubuntu/workspace/assignment1/data/'+f+'.txt', body);
         // console.log('done writing file '+ f);
       }
-      else {console.error('request failed')}
+      else {console.error('request failed')};
     });
 });
     
